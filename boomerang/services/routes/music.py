bfa7c356f454c9c072b0c8f3ca53c8b46e704c4b
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-03-26 11:25:06 -0400

    Edit to allow guest raking display

--- boomerang/services/routes/music.py
+++ boomerang/services/routes/music.py
@@ -14,7 +14,6 @@ class routeMusic():
                 return None
 
             if user_id is not None:
-                # Route for guest
                 patternstatus.append(
                     {
                         'best':{
@@ -27,6 +26,45 @@ class routeMusic():
                         }
                     }
                 )
+
+            for i in range(100):
+                weeklyTop.append(
+                    {
+                        'name': 'JoeMama',
+                        'nation': 'KR',
+                        'score': 100*i,
+                        'ranking': i
+                    }
+                )
+                
+            data = {
+                'userPatternStatus': patternstatus,
+                'weeklyTop': weeklyTop
+            }
+
+            return data, 200
+
+    class routeGuestMusicBestRankings(Resource):
+        def get(self, music_id):
+            patternstatus = []
+            weeklyTop = []
+
+            if music_id is None:
+                return None
+
+            # Route for guest
+            patternstatus.append(
+                {
+                    'best':{
+                        'score': 6969,
+                        'noteScore': 100,
+                        'maxCombo': 10,
+                        'realCombo': 20,
+                        'accuracy': 100,
+                        'rankClass': 'A',
+                    }
+                }
+            )
 
             for i in range(100):
                 weeklyTop.append(
