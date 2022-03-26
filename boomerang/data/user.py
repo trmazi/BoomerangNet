20d92891d48e3010fa6923d57db2f2d02e3c7882
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-03-26 17:09:02 -0400

    Load userAuth from SQL

--- boomerang/data/user.py
+++ boomerang/data/user.py
@@ -1,5 +1,30 @@
+from boomerang.data.sql import coreSQL
+
 class userDataHandle():
     '''
     Handler for reacting to userdata.
     '''
-    
+
+    def userFromCardID(cardid: str):
+        '''
+        Gets a user's cardID, returns user's profile and success bool.
+        '''
+        connection = coreSQL.makeConnection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM user where cardid= '{cardid}'")
+
+        result = cursor.fetchone()
+
+        if result is None:
+            return ({}, False)
+        else:
+            userid, card, banned, data = result
+            return (
+                {
+                    'id': userid,
+                    'cardid': card,
+                    'banned': banned,
+                    'data': data
+                },
+                True
+            )
--- boomerang/services/routes/auth.py
+++ boomerang/services/routes/auth.py
@@ -1,6 +1,8 @@
 from flask_restful import Resource
 from flask import request
 
+from boomerang.data.user import userDataHandle
+
 class routeAuth():
     '''
     Class for routing all authentications.
@@ -19,9 +21,24 @@ class routeAuth():
         def post(self):
             cardid = request.json['cardId']
 
-            userdata = {
-                'authorization': '',
-                'userId': cardid,
-                'isSuccess': True,
-            }
+            user, success = userDataHandle.userFromCardID(cardid)
+            if not success:
+                userdata = {
+                    'authorization': '',
+                    'userId': "NEWUSER",
+                    'isSuccess': success,
+                }
+            else:
+                if user.get('banned'):
+                    return {
+                        'authorization': '',
+                        'userId': str(user.get('id')),
+                        'isSuccess': False,
+                    }, 200
+                
+                userdata = {
+                    'authorization': '',
+                    'userId': str(user.get('id')),
+                    'isSuccess': success,
+                }
             return userdata, 200
