5cb400b4f9af3c9ba98fa8150b738c1c6931481a
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-03-29 18:27:19 -0400

    Start work on saving user

--- boomerang/services/routes/stages.py
+++ boomerang/services/routes/stages.py
@@ -1,4 +1,8 @@
 from flask_restful import Resource
+from flask import request
+
+from boomerang.data.validated import ValidatedDict
+from boomerang.data.user import userDataHandle
 
 class routeStages():
     '''
@@ -10,7 +14,23 @@ class routeStages():
             return 200
 
     class routeUserFinalHistories(Resource):
+        '''
+        This is actually where the end-game data is saved,
+        such as XP and BP. Keeping it in stages though, as that's 
+        how it's sent.
+        '''
+
         def post(self, user_id):
+            sent = ValidatedDict(request.json)
+            print(request.json)
+            user = userDataHandle.userFromUserID(int(user_id))
+            userdict = user.get_dict('data')
+
+            userdict.replace_int('points', sent.get_int('optainBeatPoint') + userdict.get_int('points'))
+            userdict.replace_int('exp', sent.get_int('optainExp') + userdict.get_int('exp'))
+
+            user.replace_dict('data', userdict)
+            userDataHandle.putUserFromUserID(user_id, user)
             return 200
 
     class routeGuestUserMusicHistories(Resource):
