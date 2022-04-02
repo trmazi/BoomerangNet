4c0fadd1a7dc5c2f73c8b2f5aa07fec58a924eed
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-04-02 14:18:38 -0400

    Rewrite almost all user webui stuff.
    This commit rewrites a LOT of stuff for the webui.

--- boomerang/utils/webui.py
+++ boomerang/utils/webui.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, send_from_directory
+from flask import Flask, render_template, request, url_for, send_from_directory, redirect
 import random
 import string
 
@@ -58,9 +58,9 @@ class BoomerangWebui():
                 username = request.form['username']
                 iconid = request.form['iconid']
                 if username == "":
-                    return "Username cannot be blank!"
+                    return render_template('status.html', func = "Create User", status = "FAILURE!", status2 = "Username cannot be blank.", redir = url_for('adduser'))
                 elif iconid == "Profile Icon":
-                    return "Please select an Icon"
+                    return render_template('status.html', func = "Create User", status = "FAILURE!", status2 = "Please set an icon.", redir = url_for('adduser'))
 
                 userdata = ValidatedDict({
                     'name':username,
@@ -77,3 +77,61 @@ class BoomerangWebui():
                 userDataHandle.createNewUser(user)
 
                 return render_template('usercreated.html', username = username, cardid = cardid)
+
+        @app.route('/web/loaduser', methods = ['GET','POST'])
+        def loaduser():
+            if request.method == 'GET':
+                return render_template('loaduser.html')
+
+            if request.method == 'POST':
+                cardid = request.form['cardid']
+                if not userDataHandle.checkForCardid(cardid):
+                    return render_template('status.html', func = "Update User", status = "FAILURE!", status2 = "The card you suppled doesn't exist.", redir = url_for('loaduser'))
+                
+                user, status = userDataHandle.userFromCardID(cardid)
+                if not status:
+                    return render_template('status.html', func = "Update User", status = "FAILURE!", status2 = "There was an issue loading your profile.", redir = url_for('loaduser'))
+
+                userdata = user.get_dict('data')
+
+                userid = user.get_int('id', None)
+                username = userdata.get_str('name', "Newcomer")
+                iconid = userdata.get_int('iconid', 1)
+
+                return redirect(url_for('edituser') + f"?username={username}&iconid={iconid}&id={userid}")
+
+        @app.route('/web/edituser', methods = ['GET', 'POST'])
+        def edituser():
+            if request.method == 'GET':
+                username = request.args.get('username')
+                iconid = request.args.get('iconid')
+                id = request.args.get('id')
+                if username == None:
+                    return "No username was sent."
+                elif iconid == None:
+                    return "No iconid was sent."
+                elif id == None:
+                    return "No id was sent."
+
+                return render_template('edituser.html', username=username, iconid=iconid, iconlist = networkDataHandle.getIconList(), userid = id)
+
+            if request.method == 'POST':
+                username = request.form['username']
+                iconid = request.form['iconid']
+                userid = request.form['userid']
+                if username == "":
+                    return render_template('status.html', func = "Update User", status = "FAILURE!", status2 = "Username cannot be blank.", redir = "")
+                elif iconid == "Profile Icon":
+                    return render_template('status.html', func = "Update User", status = "FAILURE!", status2 = "Please set an icon.", redir = "")
+
+                user = userDataHandle.userFromUserID(int(userid))
+                userdata = user.get_dict('data')
+
+                userdata.replace_int('iconid', int(iconid))
+                userdata.replace_str('name', username)
+
+                user.replace_dict('data', userdata)
+
+                userDataHandle.putUserFromUserID(int(userid), user)
+
+                return render_template('status.html', func = "Update User", status = "Success!", status2 = "You will be redirected to the userlist in 5 secs.", redir = url_for('top'))
