0e98bae86e4186edbb5da690807923c8a99ba5e8
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-05-06 16:00:45 -0400

    Update boomerang.py

--- boomerang/__init__.py
+++ boomerang/__init__.py
@@ -1,3 +1,9 @@
-import os.path
+import os
+
+path = os.path.abspath(os.path.dirname(__file__))
+name = os.path.basename(__file__)
+
+import sys
+sys.path.append(path)
 
 package_root = os.path.dirname(os.path.abspath(__file__))
